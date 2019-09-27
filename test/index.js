13908e85692ac1aef9b311186d1cf129ff89883b
Juan Carlos Moreno <juancarlos@payapi.io>
2019-09-27 16:05:04 +0200

    Added tests for credit check

--- test/index.js
+++ test/index.js
@@ -69,16 +69,48 @@ describe('PayapiApiClient', function() {
 
       expect(result).to.have.property('token', 'encodedToken');
     });
-    it('Should return a Unauthorized error if authentication failed', async () => {
+    it('Should return a Unauthorized error if authentication failed', () => {
       sinon.stub(axios, 'post').returns({ status: 401, data: { error: 'Unauthorized' } });
 
       expect(new PayapiApiClient(params).authenticate()).to.be.rejectedWith(Error, /Unauthorized/ );
     });
 
-    it('Should return a generic error if authentication failed for unknown reason', async () => {
+    it('Should return a generic error if authentication failed for unknown reason', () => {
       sinon.stub(axios, 'post').returns({ status: 504 });
 
       expect(new PayapiApiClient(params).authenticate()).to.be.rejectedWith(Error);
+    });
+  });
+
+  describe('Credit check', () => {
+    let payapiApiClient;
+
+    before(async function()  {
+      sinon.stub(axios, 'post').returns({ status: 200, data: { token: 'encodedToken' } });
+      payapiApiClient = new PayapiApiClient(params);
+      await payapiApiClient.authenticate();
+      sinon.restore();
+    });
+
+    it('Should success in credit check', async () => {
+      sinon.stub(axios, 'post').returns({ status: 200, data: { message: 'Credit check passed successfully' } });
+      const result = await payapiApiClient.creditCheck('10102403231', 1100, 'FI');
+
+      expect(result).to.have.property('message', 'Credit check passed successfully');
+    });
+    it('Should return a Unauthorized error if authentication failed', async () => {
+      sinon.stub(axios, 'post').returns({ status: 412, data: { error: 'Credit check amount must be a positive number' } });
+      const result = await payapiApiClient.creditCheck('10102403231', 1100, 'FI');
+
+      await expect(payapiApiClient.creditCheck('10102403231', 1100, 'FI'))
+        .to.be.rejectedWith(Error, /Credit check amount must be a positive number/);
+    });
+
+    it.skip('Should return Unexpected status code received if http status returned is 503', () => {
+      sinon.stub(axios, 'post').returns({ status: 503 });
+
+      expect(payapiApiClient.creditCheck('10102403231', 1100, 'FI'))
+        .to.be.rejectedWith(Error, /Unexpected status code received/);
     });
   });
 });
